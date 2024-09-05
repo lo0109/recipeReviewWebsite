33323e980e8c7178391146f1da3b837f66206cfb
SW Lo <swl@SWs-MacBook-Pro.local>
2024-09-06 00:50:00 +1000

    Finished Home Page

---
+++ webDev/resources/class/user.php
@@ -0,0 +1,28 @@
+<?php
+namespace pp;
+
+class User {
+    protected $name;
+    protected $email;
+    protected $password;
+
+    public function __construct($name, $email, $password) {
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+}
+
+?>
--- webDev/routes/web.php
+++ webDev/routes/web.php
@@ -3,5 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    $sql = "SELECT * FROM recipe";
+    $recipes = DB::select($sql);
+    return view('home', ['recipes' => $recipes]);
+});
+
+Route::get('recipe/{id}', function ($id) {
+    return view('recipe', ['id' => $id]);
+});
+
+Route::get('recipe', function () {
+
+    return view('recipe');
+    
+});
+
+Route::post('comment', function () {
+    $name = $_POST['name'];
+    $comment = $_POST['comment'];
+    $email = $_POST['email'];
+    return view('comment', ['name' => $name, 'comment' => $comment, 'email' => $email]);
 });
